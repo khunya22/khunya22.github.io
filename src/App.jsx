1a216d8fbf6a1a9271eb5471cde35c59ec25d0be
khunya22 <khunya22@proton.me>
2024-11-16 23:32:53 -0800

    fixed deployments

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css
+import Portfolio from './Portfolio";
 
 import React from 'react';
 import { styled } from '../stitches.config';
